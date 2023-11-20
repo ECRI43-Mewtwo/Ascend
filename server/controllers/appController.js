35e3c76a7b5eeb1698ee0aa589c1d42cff5d5561
SpencerHuey <spencer.huey@colorado.edu>
2023-11-20 16:43:58 -0700

    createApp and updateApp routes

---
+++ server/controllers/appController.js
@@ -0,0 +1,31 @@
+const db = require('../models/models');
+
+const appController = {};
+
+appController.createApp = async (req, res, next) => {
+  const { date, company, title, location, app_type, status, contact, notes, referral, link, starred } = req.body
+  const addAppArr = [date, company, title, location, app_type, status, contact, notes, referral, link, starred]
+  const addAppQuery = 'INSERT INTO app_table (date, company, title, location, app_type, status, contact, notes, referral, link, starred) VALUES ($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11);'
+  try {
+    db.query(addAppQuery, addAppArr).then (()=> {
+        return next();
+    })  
+  } catch (error) {
+    return next(error);
+  }
+}
+
+appController.updateApp = async (req, res, next) => {
+  const { id, date, company, title, location, app_type, status, contact, notes, referral, link, starred } = req.body
+  const updateAppArr = [date, company, title, location, app_type, status, contact, notes, referral, link, starred, id]
+  const updateAppQuery = 'UPDATE app_table SET date=$1, company=$2, title=$3, location=$4, app_type=$5, status=$6, contact=$7, notes=$8, referral=$9, link=$10, starred=$11 WHERE id=$12;'
+  try {
+    db.query(updateAppQuery, updateAppArr).then (()=> {
+        return next();
+    })  
+  } catch (error) {
+    return next(error);
+  }
+};
+
+module.exports = appController;
--- server/server.js
+++ server/server.js
@@ -1,10 +1,25 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const appController = require('./controllers/appController');
 
 const PORT = 3000;
 
 app.get('/', (req,res) => {
   return res.sendFile(path.join(__dirname, '..', 'src', 'public', 'index.html'));
 })
+
+// api/createApp
+app.post('/api', appController.createApp, (req,res) => {
+  return res.status(201).json('Successful addition to db');
+}) 
+
+// api/updateApp
+app.put('/api', appController.updateApp, (req,res) => {
+  return res.status(200).json('Successful update to db');
+})
+
 app.listen(PORT, () => console.log(`Listening on PORT: ${PORT}`));
