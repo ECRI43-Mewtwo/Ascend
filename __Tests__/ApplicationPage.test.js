d1e9c85a8408d80b2b44efd2d9da96f4f348112f
weiwang0305 <weiwang0305@gmail.com>
2023-11-20 15:55:42 -0500

    updateAppPage, ApplicationPage, HomePage components rendering

---
+++ __Tests__/ApplicationPage.test.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import {expect, jest, test} from '@jest/globals';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom';
+import '@testing-library/jest-dom/extend-expect';
+
+
+describe('Home Page Tests', () => {
+  beforeEach(() => {
+    render(
+      <ApplicationPage />
+    )
+  })
+
+  test('renders the application-page-form', async () => {
+    const applicationPageForm = await screen.querySelector('#app-page-form')
+    expect(applicationPageForm).toBeInTheDocument()
+  })
+  test('render application-page-submit-box', async () => {
+    const applicationPageSubmitBox = await screen.querySelector('#application-page-submit-box')
+  })
+})
+
+function test(){
+    console.log('test')
+}
