089dcb165056d6630a66581a0143d2a10f0bb75b
peter chung <peter.chung.716@gmail.com>
2023-11-21 15:47:57 -0500

    create test data for homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -4,10 +4,37 @@ import { useState, useEffect } from 'react';
 
 const HomePage = () => {
   const [dropdown, updateDropDown] = useState('Open');
-
+  const [appData, updateAppData] = useState([]);
   // GET request
   // cache the GET request
   // useEffect where the page re-renders with applications
+
+  const testData = [
+    {
+      date: '11/19/23',
+      company_name: 'company1',
+      job_title: 'front-end engineer',
+      location: 'remote',
+      type_of_app: 'linkedin',
+      status: 'Open',
+      contact: '',
+      notes: '',
+      referral: '',
+      link: '',
+    },
+    {
+      date: '11/21/23',
+      company_name: 'company2',
+      job_title: 'software engineer',
+      location: 'remote',
+      type_of_app: 'linkedin',
+      status: 'Open',
+      contact: '',
+      notes: '',
+      referral: '',
+      link: '',
+    },
+  ];
 
   return (
     <div>
